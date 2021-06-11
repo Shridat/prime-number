d86d9092bb067e49451441b4dd7df8d4203dc1bf
Shridat <80768804+Shridat@users.noreply.github.com>
2021-06-11 19:45:16 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 l=int(input("Enter a lower interval="))
 u=int(input("Enter a upper interval="))
 
-print("Prime numbers between "+str(l)+"and "+str(u)+"is=")
+print("Prime numbers between "+str(l)+" and "+str(u)+" is=")
 for num in range(l,u+1):
     flag=1
     if(num>1):
